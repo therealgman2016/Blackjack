959c9a72a4e37360315bc03efda349337888378c
Gabriel Moore <125038221+therealgman2016@users.noreply.github.com>
2023-08-22 09:31:05 -0400

    add to js file. DNT

---
+++ script.js
@@ -0,0 +1,26 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+
+// Build an 'original' deck of 'card' objects used to create shuffled decks
+const originalDeck = buildOriginalDeck();
+renderDeckInContainer(originalDeck, document.getElementById('original-deck-container'))
+
+
+// functions
+function buildOriginalDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        deck.push({
+          // The 'face' property maps to the library's CSS classes for cards
+          face: `${suit}${rank}`,
+          // Setting the 'value' property for game of blackjack, not war
+          value: Number(rank) || (rank === 'A' ? 11 : 10)
+        });
+      });
+    });
+    return deck;
+}
